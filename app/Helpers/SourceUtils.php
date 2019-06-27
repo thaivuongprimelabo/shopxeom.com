115cddb181f10a08614b82cabd6a9b34683f6999
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2019-06-27 22:29:00 +0700

    Source editor

--- app/Helpers/SourceUtils.php
+++ app/Helpers/SourceUtils.php
@@ -6,6 +6,12 @@ namespace App\Helpers;
 class SourceUtils {
     
     private $ignore = ['.settings', 'vendor', '.buildpath', '.', '..', '.git'];
+    private $icons = [
+        'php' => 'lang_icons/php.png',
+        'css' => 'lang_icons/css.png',
+        'html' => 'lang_icons/html.png',
+        'js' => 'lang_icons/js.png',
+    ];
     
     public static $instance;
     
@@ -23,98 +29,55 @@ class SourceUtils {
         $root = $_SERVER['DOCUMENT_ROOT'];
         $sources = [];
         $this->loadFoldersAndFiles($root, $sources);
-        $sources = array_values($sources);
-        $json = json_encode($sources);
+        $json = json_encode(array_values($sources));
         return $json;
     }
     
     public function loadFoldersAndFiles($dirPath, &$sources = []) {
         
+        $arrFolders = [];
+        $arrFiles = [];
+        
         if (!is_dir($dirPath)) {
             return true;
         }
         if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
             $dirPath .= '/';
         }
-//         $files = glob($dirPath . '*', GLOB_MARK);
         $files = scandir ($dirPath);
         foreach ($files as $indx=>$file) {
             if(in_array($file, $this->ignore)) {
                 continue;
             }
-//             $file = str_replace('\\', '/', $file);
             $filePath = $dirPath . $file;
             if (is_dir($filePath)) {
-//                 $file = substr($file, 0, -1);
-//                 $expl = explode('/', $file);
-//                 $folderName = end($expl);
-//                 $id = $folderName . $indx;
                 $node = [
                     'id' => $file . $indx . time(),
                     'text' => $file,
                     'children' => [],
-                    'type' => 'd'
                 ];
                 
-                $sources[intval($indx)] = $node;
+                $arrFolders[$indx] = $node;
                 
-                $this->loadFoldersAndFiles($filePath, $sources[intval($indx)]['children']);
+                $this->loadFoldersAndFiles($filePath, $arrFolders[$indx]['children']);
             } else {
-//                 $expl = explode('/', $file);
-//                 $fileName = end($expl);
                 $node = [
                     'id' => $file,
                     'text' => $file,
                     'icon' => 'fa fa-sticky-note-o',
                     'children' => [],
-                    'type' => 'f'
                 ];
-                array_push($sources, $node);
+
+                $ext = pathinfo($file, PATHINFO_EXTENSION);
+                if(isset($this->icons[$ext])) {
+                    $node['icon'] = url($this->icons[$ext]);
+                }
+                
+                array_push($arrFiles, $node);
             }
-        }
-    }
-    
-    public function loadFoldersAndFiles1($dirPath, &$sources = []) {
-        
-        if (!is_dir($dirPath)) {
-            return true;
-        }
-        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
-            $dirPath .= '/';
         }
         
-        $files = glob($dirPath . '*', GLOB_MARK);
-        foreach ($files as $indx=>$file) {
-            if(in_array($file, $this->ignore)) {
-                continue;
-            }
-            $file = str_replace('\\', '/', $file);
-            if (is_dir($file)) {
-                $file = substr($file, 0, -1);
-                $expl = explode('/', $file);
-                $folderName = end($expl);
-                $id = $folderName . $indx;
-                $node = [
-                    'id' => $folderName . $indx . time(),
-                    'text' => $folderName,
-                    'children' => []
-                ];
-                
-                $sources[$indx] = $node;
-                
-                $this->loadFoldersAndFiles($file, $sources[$indx]['children']);
-            } else {
-                $expl = explode('/', $file);
-                $fileName = end($expl);
-                $node = [
-                    'id' => $fileName,
-                    'text' => $fileName,
-                    'icon' => 'fa fa-sticky-note-o',
-                    'children' => []
-                ];
-                array_push($sources, $node);
-            }
-        }
+        $sources = array_merge($arrFolders, $arrFiles);
     }
 }
 ?>
